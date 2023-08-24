cbba6e8000a1720c17b597dbc7185b9ca67c5a43
Raaxxy <r195aryan@students.aryacollege.me>
2023-08-25 01:31:42 +0530

    Final commit of the chat app

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+
+const users = {};
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
@@ -13,15 +15,29 @@ app.use(express.static('public'));
 io.on('connection', (socket) => {
   console.log('A user connected');
 
+  // Ask the user for their name
+  socket.emit('request-name');
+
+  // Handle user registration with a name
+  socket.on('register-name', (name) => {
+    users[socket.id] = name;
+    io.emit('update-users', Object.values(users));
+    console.log(`${name} (${socket.id}) has joined the chat.`);
+  });
+
   // Handle incoming messages
   socket.on('message', (message) => {
-    // Broadcast the message to all connected clients
-    io.emit('message', message);
+    const userName = users[socket.id];
+    //io.emit('message', { user: userName, message }); // Send both user and message
+    io.emit('message', { user: users[socket.id], message });
   });
 
   // Handle disconnection
   socket.on('disconnect', () => {
-    console.log('A user disconnected');
+    const disconnectedUser = users[socket.id];
+    delete users[socket.id];
+    io.emit('update-users', Object.values(users));
+    console.log(`${disconnectedUser} (${socket.id}) has left the chat.`);
   });
 });
 
