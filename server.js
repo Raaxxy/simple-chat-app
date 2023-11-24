9396d0b5a8677cfcb2429de8885898df3c3b712f
Rahul <90283239+Raaxxy@users.noreply.github.com>
2023-11-24 15:33:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,31 +8,31 @@ const io = socketIO(server);
 
 
 const users = {};
-// Serve static files from the 'public' directory
+
 app.use(express.static('public'));
 
-// Handle socket connections
+
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Ask the user for their name
+ 
   socket.emit('request-name');
 
-  // Handle user registration with a name
+
   socket.on('register-name', (name) => {
     users[socket.id] = name;
     io.emit('update-users', Object.values(users));
     console.log(`${name} (${socket.id}) has joined the chat.`);
   });
 
-  // Handle incoming messages
+ 
   socket.on('message', (message) => {
     const userName = users[socket.id];
-    //io.emit('message', { user: userName, message }); // Send both user and message
+   
     io.emit('message', { user: users[socket.id], message });
   });
 
-  // Handle disconnection
+
   socket.on('disconnect', () => {
     const disconnectedUser = users[socket.id];
     delete users[socket.id];
